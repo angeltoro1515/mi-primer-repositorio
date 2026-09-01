71087a08f6b97ed12ca796172a9374f8cf583127
angeltoro1515 <angeltoro2226@gmail.com>
2026-09-01 13:14:10 -0400

    ejercicio 4 completado

--- inicio.py
+++ inicio.py
@@ -34,11 +34,20 @@ print("el area de un trangulo es", area)
 
 
 
+celsius = float( input("ingresa la temperatura en grados:"))
+
+farenheit = (celsius * 9 / 5) +32
+print("la tempertura de farenheit es:", farenheit)
 
 
 
+numero = int(input("ingresa un numero entero"))
 
+if numero % 2 == 0:
+   print("el nuemro es par")
 
+else:
+   print("el numero es impar") 
 
 
 
