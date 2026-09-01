5754083c3acb8d123fa4ef6763ffdb83b87e6bfa
angeltoro1515 <angeltoro2226@gmail.com>
2026-09-01 12:35:47 -0400

    ejercicio1 y 2 completados

---
+++ inicio de vs code.py
@@ -0,0 +1,21 @@
+print("hola mundo")
+
+num1 = float(input("ingresa el primer numero: "))
+num2 = float(input ("ingresa el segundo numero: "))
+operador = input(" ingresa el operador (+, -, *, /): ")
+
+if operador == "+":
+ resultado = num1 + num2
+print("el resultado de la suma es, resultado")
+
+elif operador == "-":
+ resultado = num1 - num2
+   print("el resultado de la resta es, resultado")
+
+elif operador == "*":
+resultado = num1 * num2
+print("el resultado de la multiplicacion es, resulrado")
+ 
+elif operador == "/":
+resultado = num1 /num2
+print("el resultado de la division es, resultado")
--- inicio.py
+++ inicio.py
@@ -1,1 +1,47 @@
 print("hola mundo")
+
+num1 = float(input("ingresa el primer numero: "))
+num2 = float(input ("ingresa el segundo numero: "))
+operador = input(" ingresa el operador (+, -, *, /): ")
+
+if operador == "+":
+ print("resultado:", num1 + num2)
+
+elif operador == "-":
+    print("resultado:", num1 - num2)
+  
+
+elif operador == "*":
+    print ("resultado:", num1 * num2)
+ 
+elif operador == "/":
+  if num2 != 0: 
+     print("resultado:", num1 /num2)
+
+  else:
+      print("no se puede dividir entre 0.")
+
+else:
+   print("operador no valido.")
+
+
+base = float( input("ingresa la base del triangulo:"))
+altura = float( input("ingresa el altura del triangilo:"))
+
+area = (base * altura) /2
+
+print("el area de un trangulo es", area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
